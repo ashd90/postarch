c086b3aae0cc11541ddde70de279c2960d128a55
Ashish <232292705+ashd90@users.noreply.github.com>
2026-01-29 08:42:15 +0530

    Updated script

--- install.sh
+++ install.sh
@@ -4,13 +4,10 @@
 #
 curl -sS https://starship.rs/install.sh | sh
 
-echo eval "$(starship init bash)" >> ~/.bashrc
+echo eval "$(starship init bash)" >>~/.bashrc
 
+sudo pacman -S --needed - <packages.txt
 
-cp alaritty.toml ~/.config/alacritty/
-
-sudo pacman -S --needed - < packages.txt
-
-yay -S --noconfirm - < aur-pkg.txt
+yay -S --noconfirm - <aur-pkg.txt
 
 cp .fonts/NotoColorEmoji-Regular.ttf ~/.local/share/fonts/
